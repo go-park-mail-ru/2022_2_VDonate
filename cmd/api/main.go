3e30f853925fc57ec4be55b7e377b5e50d87df1c
zenehu <zeronethunter2001@gmail.com>
2022-10-07 06:02:37 +0300

    [VD-43] forgot about main :)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,8 +4,7 @@ import (
 	"flag"
 	"github.com/go-park-mail-ru/2022_2_VDonate/init/system"
 	"github.com/go-park-mail-ru/2022_2_VDonate/internal/config"
-	storage "github.com/go-park-mail-ru/2022_2_VDonate/internal/storages"
-	logger "github.com/go-park-mail-ru/2022_2_VDonate/pkg/logger"
+	"github.com/labstack/echo/v4"
 	"log"
 )
 
@@ -21,22 +20,9 @@ func main() {
 		log.Fatalf("failed to open config: %s", err)
 	}
 
-	/*---------------------------logrus---------------------------*/
-	l, err := logger.NewLogrus(cfg.Server.Host, cfg.Server.Port)
-	if err != nil {
-		log.Fatalf("failed to init logrus: %s", err)
-	}
-	l.Logrus.Info("logrus initialized successfully")
-
-	/*---------------------------storage--------------------------*/
-	store := storage.New()
-	if err = store.Open(cfg.DB.Driver, cfg.DB.URL); err != nil {
-		l.Logrus.Fatalf("failed to create storage: %s", err)
-	}
-
 	/*----------------------------server--------------------------*/
-	s := system.New(store, l, cfg)
-	if err = s.Start(); err != nil {
-		s.Logger.Logrus.Fatalf("server errors: %s", err)
+	s := system.New(echo.New(), cfg)
+	if err := s.Start(); err != nil {
+		s.Echo.Logger.Error("server errors: %s", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,23 @@ module github.com/go-park-mail-ru/2022_2_VDonate
 go 1.19
 
 require (
-	github.com/gorilla/mux v1.8.0
-	github.com/lib/pq v1.10.7
-	github.com/rs/cors v1.8.2
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/labstack/echo/v4 v4.9.0
+	github.com/lib/pq v1.2.0
 	github.com/sirupsen/logrus v1.9.0
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/jinzhu/copier v0.3.5 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 )
