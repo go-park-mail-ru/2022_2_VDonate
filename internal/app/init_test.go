24f7033e6ee593f59f892c2ef0989c8af9420ae1
Ivan <ivan.milchenko.92@mail.ru>
2022-10-19 12:26:16 +0300

    [VD-37] Add checks on existance for server.

--- internal/app/init_test.go
+++ internal/app/init_test.go
@@ -1,10 +1,12 @@
 package app
 
 import (
+	"testing"
+
 	"github.com/go-park-mail-ru/2022_2_VDonate/internal/config"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
-	"testing"
+	// "github.com/stretchr/testify/assert"
 )
 
 func TestServer_Start(t *testing.T) {
@@ -12,8 +14,19 @@ func TestServer_Start(t *testing.T) {
 
 	cfg := config.New()
 	err := cfg.Open("../../configs/config_local.yaml")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Error(t, err)
+	}
 
 	server := New(e, cfg)
 	server.init()
+
+	assert.NotEqual(t, server.authHandler, nil)
+	assert.NotEqual(t, server.authMiddleware, nil)
+	assert.NotEqual(t, server.postsHandler, nil)
+	assert.NotEqual(t, server.userHandler, nil)
+	assert.NotEqual(t, server.AuthService, nil)
+	assert.NotEqual(t, server.PostsService, nil)
+	assert.NotEqual(t, server.UserService, nil)
+	assert.NotEqual(t, server.Config, nil)
 }
