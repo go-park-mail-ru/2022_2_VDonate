1cfa33ea5d30b42ae5e940ea485f519041e0efc0
Ivan <ivan.milchenko.92@mail.ru>
2022-11-01 01:32:07 +0300

    [VD-65] Add error on empty password.

--- internal/utils/hash_password.go
+++ internal/utils/hash_password.go
@@ -1,12 +1,14 @@
 package utils
 
 import (
+	"errors"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 func HashPassword(password string) (string, error) {
 	if len(password) == 0 {
-		return "", nil
+		return "", errors.New("password is empty")
 	}
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err 
