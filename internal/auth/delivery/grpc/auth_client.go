b30ae41707569dbf8475ee0ce180fafd4a2972d9
Ivan <ivan.milchenko.92@mail.ru>
2022-11-30 02:12:01 +0300

    [VD-99] Implemented auth service.

--- internal/auth/delivery/grpc/auth_client.go
+++ internal/auth/delivery/grpc/auth_client.go
@@ -2,26 +2,24 @@ package authClient
 
 import (
 	"context"
-	"time"
 
+	"github.com/go-park-mail-ru/2022_2_VDonate/internal/domain"
 	"github.com/go-park-mail-ru/2022_2_VDonate/internal/microservices/auth/protobuf"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type authServiceClient struct {
 	authClient protobuf.AuthServiceClient
 }
 
-func NewAuthClient(authClient protobuf.AuthServiceClient) authServiceClient {
+func NewAuthClient(authClient protobuf.AuthServiceClient) domain.AuthServiceManager {
 	return authServiceClient{
 		authClient: authClient,
 	}
 }
 
-func (ac authServiceClient) CreateSession(userID uint64, expires time.Time) (string, error) {
+func (ac authServiceClient) CreateSession(userID uint64) (string, error) {
 	sessionID, err := ac.authClient.CreateSession(context.Background(), &protobuf.Session{
 		UserId: userID,
-		Expires: timestamppb.New(expires),
 	})
 	if err != nil {
 		return string(""), err
@@ -48,4 +46,4 @@ func (ac authServiceClient) GetBySessionID(sessionID string) (*protobuf.Session,
 	return &protobuf.Session{
 		SessionId: session.SessionId,
 	}, nil
-} 
+}
